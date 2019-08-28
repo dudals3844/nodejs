dfd55b0606fd7aff46d540fd113a5bdf72fc9b00
young min <dudals3844@gmail.com>
2019-08-28 11:23:12 +0000

    make notice Schema

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
 
+//mongoose.connect(process.env.MONGO_DB);
+
 mongoose.connect("mongodb+srv://dudals3844:dudals34273844@cluster0-ie0jk.mongodb.net/test?retryWrites=true&w=majority");
 
 var db = mongoose.connection;
@@ -13,13 +15,41 @@ db.on("error",function(err){
     console.log("DB Error: ", err);
 });
 
+var dataSchema = mongoose.Schema({
+    name:String,
+    count:Number
+});
+
+var Data = mongoose.model('data',dataSchema);
+Data.findOne({name:"myData"}, function(err,data){
+    if(err) return console.log("Data ERROR:",err);
+    if(!data){
+        Data.create({name:"myData",count:0}, function (err,data){
+            if(err) return console.log("Data ERROR:",err);
+            console.log("Counter initialized: ",data);
+        });
+    }
+});
+
+
+
+
+
+
 app.set("view engine", 'ejs');
 app.use(express.static(__dirname+'/public'));
 
-var data = {count:0};
+//var data = {count:0};
 app.get('/', function(req, res){
-    data.count++;
-    res.render('my_first_ejs',data);
+    //https://m.blog.naver.com/azure0777/220488363644
+    Data.findOne({name:"myData"}, function(err , data){
+        if(err) return console.log("Data ERROR:",err);
+        data.count++;
+        data.save(function (err){
+            if(err) return console.log("Data ERROR:", err);
+            res.render('my_first_ejs',data);
+        });
+    });
    
 });
 
